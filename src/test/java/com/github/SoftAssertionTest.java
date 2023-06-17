a2ad367c08bec66050406f2477a8a3a5c7867123
Marena <fabriana.sands@gmail.com>
2023-06-17 14:53:19 +0500

    add test

---
+++ src/test/java/com/github/SoftAssertionTest.java
@@ -0,0 +1,42 @@
+package com.github;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SoftAssertionTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://github.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void successTest() {
+        open("/selenide/selenide");
+
+        $("#wiki-tab").click();
+        $(".markdown-body").$("[href='/selenide/selenide/wiki/SoftAssertions']")
+                .shouldHave(visible)
+                .click();
+        $("#wiki-body")
+                .shouldHave(text("3. Using JUnit5 extend test class:"));
+        $("#wiki-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
+       // sleep(5000);
+    }
+}
